84d25f1ba109b3d99a456cac08f8ba49580830be
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-06-24 05:18:26 +0300

    Update bot.js.js

--- bot.js.js
+++ bot.js.js
@@ -13093,4 +13093,17 @@ client.on('voiceStateUpdate', (voiceOld, voiceNew) => {
 });
 
 
+client.on('guildMemberAdd', member => {
+    var embed = new Discord.RichEmbed()
+    .setThumbnail(member.user.avatarURL)
+  .addField("***شكرا الانضمامك الينا***" ,member.user.username )
+    .setDescription('***بكل حب واحترام وشوق نستقبلك ونتمنى لك قضآء أجمل اللحظات ولآوقات معنا***')
+    .setColor('RANDOM')
+    .setImage('http://www.imgion.com/images/01/Welcome-buddy.jpg')
+var channel =member.guild.channels.find('name', '🙌『الـترحيب』-•')
+if (!channel) return;
+channel.send({embed : embed});
+});
+
+
 client.login(process.env.BOT_TOKEN);
